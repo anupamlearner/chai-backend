0316b6512131ae8d9fe9fe5d916bca8a069ddca3
anupamlearner <anupam.learn@gmail.com>
2026-06-09 14:10:52 +0545

    add secured routes for user account management: implement change password, get current user, and update account details endpoints

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,15 +1,21 @@
 import { Router } from "express";
 import {
+  changeCurrentPassword,
+  getCurrentUser,
+  getUserChannelProfile,
+  getWatchHistory,
   loginUser,
   logoutUser,
   registerUser,
   renewAccessToken,
+  updateAccountDetails,
   updateUserAvatar,
   updateUserCoverImage,
 } from "../controllers/user.controller.js";
 
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
+// import { verify } from "jsonwebtoken";
 
 const router = Router();
 
@@ -28,6 +34,9 @@ router.route("/login").post(loginUser);
 // SECURED ROUTES
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(renewAccessToken);
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/update-account").patch(verifyJWT, updateAccountDetails);
 
 // UPDATE AVATAR
 router
@@ -49,5 +58,9 @@ router
 router
   .route("/cover-image")
   .patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
+
+router.route("/c/:foundUser").get(verifyJWT, getUserChannelProfile);
+
+router.route("/history").get(verifyJWT, getWatchHistory);
 
 export default router;
